ed4172da344abe3b6a7616ab2f8bac1d116bf816
David Noble <davidnoble37@gmail.com>
2017-08-07 14:07:28 -0700

    api key

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const
     name: {type: String, require: true},
     email: {type: String, require: true},
     password: {type: String, require: true},
+    zipcode:{type: String, require: true},
     favoriteWines: [],
     favoriteCheeses: []
   })
